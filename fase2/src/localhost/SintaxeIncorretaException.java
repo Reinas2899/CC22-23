ec3f48a0672d9f685ba838f45e196f153c3ad991
joaocasr <castrojoao839@gmail.com>
2023-01-21 15:57:03 +0000

    .

--- fase2/src/localhost/SintaxeIncorretaException.java
+++
@@ -1,8 +0,0 @@
-/**
- * Autor: João Castro
- * Modificado: 20/out/2022
- * Descrição:
- * */
-public class SintaxeIncorretaException extends Exception{
-     public SintaxeIncorretaException(String msg){super(msg);}
-}
